4d4c2f643a3ac28f54665d038781406eca55dba7
MykolCreates <115617982+MykolCreates@users.noreply.github.com>
2023-05-01 20:07:27 +0800

    updated component issue

--- components/GenerateComponents/ContentDetails/index.tsx
+++ components/GenerateComponents/ContentDetails/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { IoCreate as Create } from "react-icons/io5";
 import { AiOutlineSave as Save, AiOutlineCopy as Copy } from "react-icons/ai";
-import { Content } from "@/pages/generate";
+// import { Content } from "@/pages/generate";
 
 interface Props {
   // content: Content;
--- pages/generate/index.tsx
+++ pages/generate/index.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 export default function Generate() {
 
   return <main></main>;
