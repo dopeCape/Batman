f5f579c40bdbe23815bd328d7dabe7312173aa6a
MykolCreates <115617982+MykolCreates@users.noreply.github.com>
2023-05-02 16:22:23 +0800

    removed title props on Tiktok

--- components/GenerateComponents/PlatformContent/Tiktok/index.tsx
+++ components/GenerateComponents/PlatformContent/Tiktok/index.tsx
@@ -4,10 +4,9 @@ import { AiOutlineSave as Save, AiOutlineCopy as Copy } from "react-icons/ai";
 
 interface Props {
   content: string;
-  title: string;
 }
 
-const Tiktok = ({ content,title }: Props) => {
+const Tiktok = ({ content }: Props) => {
   return <div>
     <p className="text-black w-[90%] text-justify">{content}</p>
     </div>;
