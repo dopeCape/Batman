a33cc41eebd3a23dcfe7d3aef9afdcad5625969d
MykolCreates <115617982+MykolCreates@users.noreply.github.com>
2023-05-07 13:34:36 +0800

    removed getServerSession

--- pages/auth/login/index.tsx
+++ pages/auth/login/index.tsx
@@ -9,15 +9,6 @@ import { authOptions } from "../../api/auth/[...nextauth]";
 import Link from "next/link";
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
-  const session = await getServerSession(context.req, context.res, authOptions);
-
-  // If the user is already logged in, redirect.
-  // Note: Make sure not to redirect to the same page
-  // To avoid an infinite loop!
-  if (session) {
-    return { redirect: { destination: "/" } };
-  }
-
   const providers = await getProviders();
 
   return {
