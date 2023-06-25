46f75f11164671cc8f2b0b69d18773dffe2d3fa7
Arijit <bhowmikzar@gmail.com>
2023-06-25 15:13:58 +0530

    Updated Home Page

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -7,6 +7,7 @@ import InstagramIcon from "@material-ui/icons/Instagram";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import TwitterIcon from "@material-ui/icons/Twitter";
 import { Button } from "@material-ui/core";
+import Link from "next/link";
 
 const socialMediaPlatforms = [
   {
@@ -50,45 +51,58 @@ const youtubeContent = [
   {
     id: "m1",
     title: "YouTube is a great platform",
+    link: "/generate/youtube",
     content:
       "Twitter is an online social networking and microblogging platform where users can post and interact with messages known as 'tweets'. It is a popular platform for real-time information and social networking.",
   },
   {
     id: "m1",
     title: "YouTube is a great platform",
+    link: "/generate/instagram",
     content:
       "Twitter is an online social networking and microblogging platform where users can post and interact with messages known as 'tweets'. It is a popular platform for real-time information and social networking.",
   },
   {
     id: "m1",
     title: "YouTube is a great platform",
+    link: "/generate/youtube",
     content:
       "Twitter is an online social networking and microblogging platform where users can post and interact with messages known as 'tweets'. It is a popular platform for real-time information and social networking.",
   },
+  //   {
+  //     id: "m1",
+  //     title: "YouTube is a great platform",
+  //     content:
+  //       "Twitter is an online social networking and microblogging platform where users can post and interact with messages known as 'tweets'. It is a popular platform for real-time information and social networking.",
+  //   },
 ];
 
 const tiktokContent = [
   {
     id: "m1",
     title: "Tiktok is a great platform",
+    link: "/generate/youtube",
     content:
       "TikTok is a short-form video-sharing platform known for its creative and entertaining content. Users can create and share short videos with various effects, filters, and music.",
   },
   {
     id: "m1",
     title: "Tiktok is a great platform",
+    link: "/generate/instagram",
     content:
       "Instagram is a photo and video-sharing social networ platform. Users can post photos and videos, follow other users, explore content, and interact through comments and likes.",
   },
   {
     id: "m1",
     title: "Tiktok is a great platform",
+    link: "/generate/youtube",
     content:
       "TikTok is a short-form video-sharing platform known for its creative and entertaining content. Users can create and share short videos with various effects, filters, and music.",
   },
   {
     id: "m1",
     title: "Tiktok is a great platform",
+    link: "/generate/instagram",
     content:
       "Instagram is a photo and video-sharing social networ platform. Users can post photos and videos, follow other users, explore content, and interact through comments and likes.",
   },
@@ -98,24 +112,28 @@ const InstagramContent = [
   {
     id: "m1",
     title: "Instagram is a great platform",
+    link: "/generate/tiktok",
     content:
       "Instagram is a short-form video-sharing platform known for its creative and entertaining content. Users can create and share short videos with various effects, filters, and music.",
   },
   {
     id: "m2",
     title: "Instagram is a great platform",
+    link: "/generate/instagram",
     content:
       "Instagram is a short-form video-sharing platform known for its creative and entertaining content. Users can create and share short videos with various effects, filters, and music.",
   },
   {
     id: "m1",
     title: "Instagram is a great platform",
+    link: "/generate/youtube",
     content:
       "Instagram is a short-form video-sharing platform known for its creative and entertaining content. Users can create and share short videos with various effects, filters, and music.",
   },
   {
     id: "m2",
     title: "Instagram is a great platform",
+    link: "/generate/instagram",
     content:
       "Instagram is a short-form video-sharing platform known for its creative and entertaining content. Users can create and share short videos with various effects, filters, and music.",
   },
@@ -125,54 +143,79 @@ const linkedInContent = [
   {
     id: "m2",
     title: "LinkedIn is a great platform",
+    link: "/generate/tiktok",
     content:
       "LinkedIn is a professional networking platform where users can create a profile, connect with colleagues and professionals, and share content related to their career and industry.",
   },
   {
     id: "m2",
     title: "LinkedIn is a great platform",
+    link: "/generate/youtube",
     content:
       "LinkedIn is a professional networking platform where users can create a profile, connect with colleagues and professionals, and share content related to their career and industry.",
   },
   {
     id: "m2",
     title: "LinkedIn is a great platform",
+    link: "/generate/tiktok",
     content:
       "LinkedIn is a professional networking platform where users can create a profile, connect with colleagues and professionals, and share content related to their career and industry.",
   },
   {
     id: "m2",
     title: "LinkedIn is a great platform",
+    link: "/generate/youtube",
     content:
       "LinkedIn is a professional networking platform where users can create a profile, connect with colleagues and professionals, and share content related to their career and industry.",
   },
 ];
 
 const useStyles = makeStyles((theme) => ({
+  containCard: {
+    display: "flex",
+    justifyContent: "center",
+  },
+
   card: {
     margin: "15px ",
     padding: "15px",
     display: "flex",
     flexDirection: "column",
     justifyContent: "center",
-    alignItems: "center",
+  },
+
+  subCard: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignContent: "center",
+    maxWidth: "1920px",
   },
 
   cardContent: {
     textAlign: "center",
-    margin: "20px 2px",
+    margin: "20px 10px",
     backgroundColor: "white",
     color: "black",
     borderRadius: theme.spacing(2),
     padding: theme.spacing(3),
-    maxWidth: "350px",
+    minWidth: "300px",
+    maxWidth: "300px",
     boxShadow: "0 6px 12px rgba(0, 0, 0, 0.4)",
   },
   descriptionCard: {
     display: "flex",
     flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    flexWrap: "wrap",
     [theme.breakpoints.up(600)]: {
       flexDirection: "row",
+      alignItems: "start",
+    },
+    [theme.breakpoints.up(960)]: {
+      flexDirection: "row",
+      justifyContent: "start",
     },
   },
 
@@ -197,8 +240,8 @@ const Home: NextPage = () => {
   const classes = useStyles();
 
   return (
-    <Grid>
-      <Grid>
+    <div className={classes.containCard}>
+      <Grid className={classes.subCard}>
         <Card className={classes.card}>
           <Typography variant="h6" component="h2" className={classes.iconCard}>
             <YouTubeIcon />
@@ -208,12 +251,19 @@ const Home: NextPage = () => {
             {youtubeContent.map((content) => {
               return (
                 <CardContent key={content.id} className={classes.cardContent}>
-                  <Typography variant="body2" className={classes.cardItemTitle}>
-                    {content.title}
-                  </Typography>
-                  <Typography variant="body2" color="textPrimary">
-                    {content.content}
-                  </Typography>
+                  <button>
+                    <Link href={content.link}>
+                      <Typography
+                        variant="body2"
+                        className={classes.cardItemTitle}
+                      >
+                        {content.title}
+                      </Typography>
+                      <Typography variant="body2" color="textPrimary">
+                        {content.content}
+                      </Typography>
+                    </Link>
+                  </button>
                 </CardContent>
               );
             })}
@@ -228,12 +278,19 @@ const Home: NextPage = () => {
             {tiktokContent.map((content) => {
               return (
                 <CardContent key={content.id} className={classes.cardContent}>
-                  <Typography variant="body2" className={classes.cardItemTitle}>
-                    {content.title}
-                  </Typography>
-                  <Typography variant="body2" color="textPrimary">
-                    {content.content}
-                  </Typography>
+                  <button>
+                    <Link href={content.link}>
+                      <Typography
+                        variant="body2"
+                        className={classes.cardItemTitle}
+                      >
+                        {content.title}
+                      </Typography>
+                      <Typography variant="body2" color="textPrimary">
+                        {content.content}
+                      </Typography>
+                    </Link>
+                  </button>
                 </CardContent>
               );
             })}
@@ -248,12 +305,19 @@ const Home: NextPage = () => {
             {linkedInContent.map((content) => {
               return (
                 <CardContent key={content.id} className={classes.cardContent}>
-                  <Typography variant="body2" className={classes.cardItemTitle}>
-                    {content.title}
-                  </Typography>
-                  <Typography variant="body2" color="textPrimary">
-                    {content.content}
-                  </Typography>
+                  <button>
+                    <Link href={content.link}>
+                      <Typography
+                        variant="body2"
+                        className={classes.cardItemTitle}
+                      >
+                        {content.title}
+                      </Typography>
+                      <Typography variant="body2" color="textPrimary">
+                        {content.content}
+                      </Typography>
+                    </Link>
+                  </button>
                 </CardContent>
               );
             })}
@@ -268,12 +332,19 @@ const Home: NextPage = () => {
             {tiktokContent.map((content) => {
               return (
                 <CardContent key={content.id} className={classes.cardContent}>
-                  <Typography variant="body2" className={classes.cardItemTitle}>
-                    {content.title}
-                  </Typography>
-                  <Typography variant="body2" color="textPrimary">
-                    {content.content}
-                  </Typography>
+                  <button>
+                    <Link href={content.link}>
+                      <Typography
+                        variant="body2"
+                        className={classes.cardItemTitle}
+                      >
+                        {content.title}
+                      </Typography>
+                      <Typography variant="body2" color="textPrimary">
+                        {content.content}
+                      </Typography>
+                    </Link>
+                  </button>
                 </CardContent>
               );
             })}
@@ -288,19 +359,26 @@ const Home: NextPage = () => {
             {InstagramContent.map((content) => {
               return (
                 <CardContent key={content.id} className={classes.cardContent}>
-                  <Typography variant="body2" className={classes.cardItemTitle}>
-                    {content.title}
-                  </Typography>
-                  <Typography variant="body2" color="textPrimary">
-                    {content.content}
-                  </Typography>
+                  <button>
+                    <Link href={content.link}>
+                      <Typography
+                        variant="body2"
+                        className={classes.cardItemTitle}
+                      >
+                        {content.title}
+                      </Typography>
+                      <Typography variant="body2" color="textPrimary">
+                        {content.content}
+                      </Typography>
+                    </Link>
+                  </button>
                 </CardContent>
               );
             })}
           </div>
         </Card>{" "}
       </Grid>
-    </Grid>
+    </div>
   );
 };
 
