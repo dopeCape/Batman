c3305c143ffd9a16d5901f4396299a0bc0998454
Mr. Robot <gomexxxx71@gmail.com>
2023-07-11 15:52:07 +0600

    add contact form data in db

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -1,62 +1,109 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import EmailIcon from "@mui/icons-material/Email";
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import { collection, addDoc } from "firebase/firestore";
+import { db } from "@/firebase";
 import classes from "./contact.module.css";
 
 export default function Contact() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
 
-  const [name , setName ] = useState('')
+  const addData = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (!name.trim().length || !email.trim().length || !message.trim().length)
+      return;
+    try {
+      await addDoc(collection(db, "contactUs"), {
+        name,
+        email,
+        message,
+      });
+      setName("");
+      setEmail("");
+      setMessage("");
+    } catch (ex) {
+      console.log("Something went wrong", ex);
+    }
+  };
+
   return (
     <div className={`${classes.contact__container} bg-gray-50`}>
-      <div className={`${classes.contact_confirm} bg-gray-200 flex flex-col items-center pt-14 px-5`}>
-        <h1 className='font-medium text-3xl text-blue-800'>Ready to create some awesome content?</h1>
-        <p className='text-gray-500 py-5 text-left text-lg'>We'd love to hear back from you and help you to generate the best content. Please get in touch with us by dropping an email or filling out the form.</p>
-        <div className='flex flex-row self-start'>
-          <EmailIcon color='primary' className='self-start mr-2'></EmailIcon>
-          <p className=' underline  text-gray-600'>contact@metridash.com</p>
+      <div
+        className={`${classes.contact_confirm} bg-gray-200 flex flex-col items-center pt-14 px-5`}
+      >
+        <h1 className="font-medium text-3xl text-blue-800">
+          Ready to create some awesome content?
+        </h1>
+        <p className="text-gray-500 py-5 text-left text-lg">
+          We'd love to hear back from you and help you to generate the best
+          content. Please get in touch with us by dropping an email or filling
+          out the form.
+        </p>
+        <div className="flex flex-row self-start">
+          <EmailIcon color="primary" className="self-start mr-2"></EmailIcon>
+          <p className=" underline  text-gray-600">contact@metridash.com</p>
         </div>
       </div>
-      <div className={`${classes.contact__form} items-center bg-white text-left`}>
-        <h1 className='font-medium text-2xl text-blue-800 mb-4'>Contact Us</h1>
-        <TextField
-          sx={{
-            marginTop: 1,
-          }}
-          id="outlined-controlled"
-          className='w-8/12'
-          label="Name"
-          value={name}
-          onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-            setName(event.target.value);
-          }}
-        />
-        <TextField
-           sx={{
-            marginTop: 1,
-          }}
-          id="outlined-controlled"
-          className='w-8/12 rounded-xl'
-          label="Email"
-          value={name}
-          onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-            setName(event.target.value);
-          }}
-        />
-        <TextField
-          sx={{
-            marginTop: 1,
-            marginBottom:1
-          }}
-          id="outlined-multiline-static"
-          label="message"
-          multiline
-          rows={4}
-          className='w-8/12 my-1'
-          // defaultValue="Default Value"
-        />
-        <Button className={classes.contact_btn} variant="contained">Submit</Button>
+      <div
+        className={`${classes.contact__form} items-center bg-white text-left`}
+      >
+        <h1 className="font-medium text-2xl text-blue-800 mb-4">Contact Us</h1>
+        <form onSubmit={addData}>
+          <TextField
+            sx={{
+              marginTop: 1,
+            }}
+            id="outlined-controlled"
+            className="w-8/12"
+            label="Name"
+            value={name}
+            onChange={(event) => {
+              setName(event.target.value);
+            }}
+            fullWidth
+          />
+          <TextField
+            sx={{
+              marginTop: 1,
+            }}
+            id="outlined-controlled"
+            className="w-8/12 rounded-xl"
+            label="Email"
+            type="email"
+            value={email}
+            fullWidth
+            onChange={(event) => {
+              setEmail(event.target.value);
+            }}
+          />
+          <TextField
+            sx={{
+              marginTop: 1,
+              marginBottom: 1,
+              display: "block",
+            }}
+            id="outlined-multiline-static"
+            label="message"
+            multiline
+            rows={4}
+            value={message}
+            onChange={(event) => setMessage(event.target.value)}
+            className="w-8/12 my-1"
+            fullWidth
+            // defaultValue="Default Value"
+          />
+          <Button
+            type="submit"
+            className={classes.contact_btn}
+            variant="contained"
+          >
+            Submit
+          </Button>
+        </form>
       </div>
     </div>
-  )
+  );
 }
