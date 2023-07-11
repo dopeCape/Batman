604e670b9199cf8816dffb32364f4383e1bab3f7
kanakdesai <kanakdesai1@gmail.com>
2023-07-11 11:51:18 +0530

    changes in tokens

--- components/GPTResponse.tsx
+++ components/GPTResponse.tsx
@@ -3,6 +3,8 @@ import { useAtom } from "jotai";
 import { useEffect, useState } from "react";
 import { generateRealTimeToken, getRealTimeToken, getUserToken } from "../auth";
 import { auth } from "@/firebase";
+import Image from "next/image";
+import tokens from '../public/icons/coins.png'
 
 export default function GPTResponse() {
   const [response] = useAtom(responseAtom);
@@ -18,12 +20,20 @@ export default function GPTResponse() {
 
   return (
     <div className="bg-white p-12 w-full max-w-screen h-screen">
-      <div className="flex flex-row ">
+      <div className="flex flex-row justify-between  ">
         <h1 className="text-gray-800 text-2xl px-5 font-sans font-medium">
           Output
         </h1>
-        <div className=" items-center justify-center flex px-4 py-1">
-          <h1 id="tokenShow">Tokens {Number(token)}</h1>
+        <div className=" items-center justify-center flex px-4  bg-gray-200 rounded-xl mr-6 py-2 ">
+          <Image
+            src={tokens}
+            alt="token"
+            width={30}
+            height={30}
+            className="mr-2"
+            
+          ></Image>
+          <h1 id="tokenShow" className="text-black"> {Number(token)} tokens</h1>
         </div>
       </div>
       <div className="flex mx-5 my-10 bg-gray-200 px-5 py-5 rounded-md">
