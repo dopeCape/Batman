ef6d00f1b96e308bdf1da53b19761fda74564b96
Mr. Robot <gomexxxx71@gmail.com>
2023-07-11 11:25:16 +0600

    get realtime token

--- components/GPTResponse.tsx
+++ components/GPTResponse.tsx
@@ -1,44 +1,36 @@
-
 import { responseAtom } from "@/utils/store";
 import { useAtom } from "jotai";
-import { useEffect , useState} from 'react';
-import { generateRealTimeToken, getRealTimeToken, getUserToken } from '../auth';
+import { useEffect, useState } from "react";
+import { generateRealTimeToken, getRealTimeToken, getUserToken } from "../auth";
 import { auth } from "@/firebase";
 
-  
 export default function GPTResponse() {
   const [response] = useAtom(responseAtom);
-  const [token, setToken] = useState(0)
-  const user = auth.currentUser
-
- 
+  const [token, setToken] = useState(0);
+  const user = auth.currentUser;
 
   useEffect(() => {
     (async () => {
-      const tk = await getUserToken(user);
+      const tk = await generateRealTimeToken(user);
       setToken(Number(tk));
-      console.log("++++++++++ this is tks " + token);
     })();
-  }, []);
- 
-
-  
+  }, [response]);
 
   return (
     <div className="bg-white p-12 w-full max-w-screen h-screen">
       <div className="flex flex-row ">
-        <h1 className="text-gray-800 text-2xl px-5 font-sans font-medium">Output</h1>
-        <div className=" bg-black items-center justify-center flex px-4 py-1">
-          <h1 id="tokenShow">Tokens  {Number(token)}</h1>
+        <h1 className="text-gray-800 text-2xl px-5 font-sans font-medium">
+          Output
+        </h1>
+        <div className=" items-center justify-center flex px-4 py-1">
+          <h1 id="tokenShow">Tokens {Number(token)}</h1>
         </div>
       </div>
       <div className="flex mx-5 my-10 bg-gray-200 px-5 py-5 rounded-md">
-      <p className="text-black">
-        {response ? response : "Response goes here..."}
-      </p>
-
+        <p className="text-black">
+          {response ? response : "Response goes here..."}
+        </p>
       </div>
     </div>
   );
 }
-
