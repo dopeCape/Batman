235adaa646be9bbc009d903dc61a6db68112f8c6
Mr. Robot <gomexxxx71@gmail.com>
2023-07-12 13:52:15 +0600

    fix token error after reloading page

--- components/GPTResponse.tsx
+++ components/GPTResponse.tsx
@@ -4,20 +4,26 @@ import { useEffect, useState } from "react";
 import { generateRealTimeToken, getRealTimeToken, getUserToken } from "../auth";
 import { auth } from "@/firebase";
 import Image from "next/image";
-import tokens from '../public/icons/coins.png'
+import tokens from "../public/icons/coins.png";
 
 export default function GPTResponse() {
   const [response] = useAtom(responseAtom);
   const [token, setToken] = useState(0);
-  const user = auth.currentUser;
+  const [user, setUser] = useState<null | any>(null);
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      setUser(user);
+    });
+  }, [user]);
 
   useEffect(() => {
     (async () => {
       const tk = await generateRealTimeToken(user);
       setToken(Number(tk));
-      console.log(JSON.stringify(response.split("\n")))
+      console.log(JSON.stringify(response.split("\n")));
     })();
-  }, [response]);
+  }, [response, user]);
 
   return (
     <div className="bg-white p-12 w-full max-w-screen h-screen overflow-scroll">
@@ -32,29 +38,37 @@ export default function GPTResponse() {
             width={30}
             height={30}
             className="mr-2"
-            
           ></Image>
-          <h1 id="tokenShow" className="text-black"> {Number(token)} Tokens</h1>
+          <h1 id="tokenShow" className="text-black">
+            {" "}
+            {Number(token)} Tokens
+          </h1>
         </div>
       </div>
-      {response ? response.split('\n').filter(e=>e).map((e, i)=>
-            {if(e){
+      {response ? (
+        response
+          .split("\n")
+          .filter((e) => e)
+          .map((e, i) => {
+            if (e) {
               return (
-              <div className={`flex mx-5 ${e.match(/[0-9]\./) ? "mb-2" : "mb-10"} ${i==0? "mt-10": "mt-0"} bg-gray-200 px-5 py-5 rounded-md `}>
-                <p className="text-black">
-                {e}
-                </p>
-             </div>
-              )
+                <div
+                  className={`flex mx-5 ${
+                    e.match(/[0-9]\./) ? "mb-2" : "mb-10"
+                  } ${
+                    i == 0 ? "mt-10" : "mt-0"
+                  } bg-gray-200 px-5 py-5 rounded-md `}
+                >
+                  <p className="text-black">{e}</p>
+                </div>
+              );
             }
-            
-           }
-          ) :  <div className="flex mx-5 my-10 bg-gray-200 px-5 py-5 rounded-md">
-          <p className="text-black">
-            response goes here
-          </p>
-        </div>}
-     
+          })
+      ) : (
+        <div className="flex mx-5 my-10 bg-gray-200 px-5 py-5 rounded-md">
+          <p className="text-black">response goes here</p>
+        </div>
+      )}
     </div>
   );
 }
