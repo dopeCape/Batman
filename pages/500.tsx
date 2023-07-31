a6e953dc62bc3ed7ec457388d367077aa4046d86
Mr. Robot <gomexxxx71@gmail.com>
2023-07-31 16:17:41 +0600

    build custom 500 page

---
+++ pages/500.tsx
@@ -0,0 +1,9 @@
+const Custom500 = () => {
+  return (
+    <h3 className="font-semibold mt-4 text-5xl flex justify-center items-center min-h-[calc(100vh-8rem)]">
+      Sorry, Server-side error occurred
+    </h3>
+  );
+};
+
+export default Custom500;
