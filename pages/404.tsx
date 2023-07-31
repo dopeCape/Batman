400c706044d9e75244542dec11ac78ff6ca166c9
Mr. Robot <gomexxxx71@gmail.com>
2023-07-31 16:14:41 +0600

    build custom 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,9 @@
+const Custom404 = () => {
+  return (
+    <h3 className="font-semibold mt-4 text-5xl flex justify-center items-center min-h-[calc(100vh-8rem)]">
+      Sorry, Page Not Found
+    </h3>
+  );
+};
+
+export default Custom404;
