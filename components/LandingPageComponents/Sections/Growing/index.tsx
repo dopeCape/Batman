77fbb7637fefb386530862f28ad217284339cab5
Mr. Robot <gomexxxx71@gmail.com>
2023-08-01 22:45:55 +0600

    fix styling

--- components/LandingPageComponents/Sections/Growing/index.tsx
+++ components/LandingPageComponents/Sections/Growing/index.tsx
@@ -24,7 +24,7 @@ const Growing = () => {
       className="flex flex-col justify-center items-center gap-y-20 py-10 relative top-[100px] transition-all duration-1000 ease-in-out bg-black"
       animate={controls}
     >
-      <h1 className="font-semibold text-[48px] leading-[54px] text-center text-transparent bg-clip-text bg-gradient-to-r from-[#009FFD] to-[#2A2A72] ">
+      <h1 className="font-semibold text-[48px] leading-[54px] text-center text-transparent bg-clip-text bg-gradient-to-r from-[#009FFD] to-[#2A2A72] pb-2">
         Never stop growing!
       </h1>
       <div className="flex gap-x-5 md:flex-row flex-col justify-center align-center">
