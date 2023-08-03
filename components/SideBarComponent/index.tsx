229f880d0572b0627dc01041d66602106d2593cc
kanakdesai <kanakdesai1@gmail.com>
2023-08-03 15:52:25 +0530

    sidebar dev

--- components/SideBarComponent/index.tsx
+++ components/SideBarComponent/index.tsx
@@ -8,6 +8,8 @@ import ExpandLess from '@mui/icons-material/ExpandLess';
 import ExpandMore from '@mui/icons-material/ExpandMore';
 import Image from 'next/image';
 import platforms from '@/data/sideBarPlatforms';
+import SearchIcon from '@mui/icons-material/Search';
+import { Input } from '@mui/material';
 // import InstagramIcon from '../../public/platformIcons/InstagramIcon.svg'
 export default function SideBar() {
 
@@ -22,22 +24,30 @@ export default function SideBar() {
         setOpenStates(newOpenStates);
     };
     return (
-        <div className='flex w-full bg-slate-300 h-full '>
-            <div className='flex w-1/3 h-full bg-[#1B1D21] flex-col'>
+        <div className='flex w-full bg-black h-full '>
+            <div className='flex w-1/3 h-full bg-[#1B1D21] flex-col overflow-scroll'>
+            <div className='h-8 flex flex-row mt-4 mx-4 bg-[#232529] rounded-md'>
+                <SearchIcon className='text-white my-1 ml-2' />
+                <input className=' bg-[#232529] rounded-md text-white outline-none pl-2' placeholder='Search' />
+                
+               
+            </div>
                 {platforms.map((platform, i) => (
+                     
                     <List key={i}>
+                      
                         <ListItemButton className='flex flex-row' onClick={() => handleClick(i)}>
                             <ListItemIcon className='flex flex-row justify-center ' >
-                                <Image alt='' width={20} height={20} src={platform.icon} />
+                                <Image alt='' width={22} height={22} src={platform.icon} />
                             </ListItemIcon>
-                            <ListItemText className='text-[#A7A7A7] justify-center' primary={platform.name} />
+                            <h1 className='text-white justify-center text-md'  >{platform.name}</h1>
                             {openStates[i] ? <ExpandLess htmlColor='#FFFFFF' /> : <ExpandMore htmlColor='#FFFFFF' />}
                         </ListItemButton>
                         <Collapse in={openStates[i]} timeout="auto" unmountOnExit>
                             <List component="div" disablePadding>
                                 {platform.items.map((item, j) => (
                                     <button className='flex w-full items-end  pl-14 ' key={j}>
-                                        <h1 className=' text-[#A7A7A7] pl-4  text-sm py-2 flex-row flex w-full text-left font-medium hover:border-r-2 border-gray-50 hover:bg-[#232529]' >{item}</h1>
+                                        <h1 className=' text-white pl-4 py-2 flex-row flex w-full text-left mb-4 text-md hover:border-r-2 border-gray-50 hover:bg-[#232529]' >{item}</h1>
                                     </button>
                                 ))}
                             </List>
@@ -45,6 +55,7 @@ export default function SideBar() {
                     </List>
                 ))}
             </div>
+            <div className='flex w-2/3 bg-[#232529]'></div>
         </div>
     )
 }
--- data/sideBarPlatforms.ts
+++ data/sideBarPlatforms.ts
@@ -8,38 +8,38 @@ import FacebookIcon from '../public/platformIcons/Facebook.png';
 const platforms = [
     {
       name: "Youtube",
-      icon: '/platformIcons/YouTube.png',
+      icon: '/platformIcons/youtubeHQ.png',
       color: "#FF0000",
       items: ["Youtube Video", "Youtube Shorts", "Youtube Title","Youtube Descriptions", "Youtube Tags", "Youtube Thumbnail"]
     },
     {
       name: "Twitter",
-      icon: '/platformIcons/Twitter.png',
+      icon: '/platformIcons/twitterHQ.png',
       color: "#1DA1F2",
       items: ["Item 4", "Item 5"]
     },
     {
       name: "Instagram",
-      icon: '/platformIcons/Instagram.png',
+      icon: '/platformIcons/instagramHiQ.png',
       color: "#C13584",
       items: ["Item 6"]
     },
     {
       name: "Linkedin",
-      icon: '/platformIcons/LinkedIn.png',
+      icon: '/platformIcons/linkednHQ.png',
       color: "#0077B5",
       items: ["LnkeDi", "Item 8", "Item 9", "Item 10"]
     },
     
     {
       name: "Facebook",
-      icon: '/platformIcons/Facebook.png',
+      icon: '/platformIcons/facebookHiQ.png',
       color: "#4267B2",
       items: ["Item 7", "Item 8", "Item 9", "Item 10"]
     },
     {
         name: "Tiktok",
-        icon: '/platformIcons/TikTokPng.png',
+        icon: '/platformIcons/tiktokHQ.png',
         color: "#FE2C55",
         items: ["Item 7", "Item 8", "Item 9", "Item 10"]
       }
