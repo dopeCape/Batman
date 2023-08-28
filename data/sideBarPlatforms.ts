cdcd82a15db460a09c23f5543337942dcc5c64f0
Mr. Robot <gomexxxx71@gmail.com>
2023-08-28 15:16:45 +0600

    improve ui

--- data/sideBarPlatforms.ts
+++ data/sideBarPlatforms.ts
@@ -1,55 +1,71 @@
-import TwitterIcon from '/public/platformIcons/Twitter.png';
-import TikTokIcon from '../public/platformIcons/TikTokPng.png';
-import YouTubeIcon from '../public/platformIcons/YouTube.png';
-import InstagramIcon from '../public/platformIcons/Instagram.png';
-import LinkedInIcon from '../public/platformIcons/LinkedIn.png';
-import FacebookIcon from '../public/platformIcons/Facebook.png';
+import TwitterIcon from "/public/platformIcons/Twitter.png";
+import TikTokIcon from "../public/platformIcons/TikTokPng.png";
+import YouTubeIcon from "../public/platformIcons/YouTube.png";
+import InstagramIcon from "../public/platformIcons/Instagram.png";
+import LinkedInIcon from "../public/platformIcons/LinkedIn.png";
+import FacebookIcon from "../public/platformIcons/Facebook.png";
 
 const platforms = [
-    {
-      name: "Youtube",
-      icon: '/platformIcons/youtube1.png',
-      color: "#FF0000",
-      
-      items: ["Youtube Video", "Youtube Short", "Youtube Title","Youtube Description", "Youtube Tag", "Youtube Thumbnail"]
-    },
-    {
-      name: "Twitter",
-      icon: '/platformIcons/twitterHQ.png',
-      color: "#1DA1F2",
-      items: ["Twitter Tweet", "Twitter Hashtag", "Twitter Bio"]
-    },
-    {
-      name: "Instagram",
-      icon: '/platformIcons/instagramHiQi.png',
-      color: "#C13584",
-      items: ["Instagram Reel", "Instagram Caption", "Instagram Bio", "Instagram Hashtag"]
-    },
-    {
-      name: "Linkedin",
-      icon: '/platformIcons/linkednHQ.png',
-      color: "#0077B5",
-      items: ["LinkedIn Post", "LinkedIn Profile Optimization", "LinkedIn Articles"]
-    },
-    
-    {
-      name: "Facebook",
-      icon: '/platformIcons/facebookHiQ.png',
-      color: "#4267B2",
-      items: ["Facebook Post", "Facebook Ad Copy", "Facebook Page Description"]
-    },
-    {
-        name: "TikTok",
-        icon: '/platformIcons/tiktokHQ.png',
-        color: "#FE2C55",
-        items: ["TikTok Video", "TikTok Hashtag", "TikTok Caption"]
-      },
-      // {
-      //   name: "General",
-      //   icon: '/platformIcons/general.png',
-      //   color: "#FE2C55",
-      //   items: ["Rewrite", "Repurpose"]
-      // }
-  ];
+  {
+    name: "YouTube",
+    icon: "/platformIcons/youtube1.png",
+    color: "#FF0000",
 
-  export default platforms;
+    items: [
+      "YouTube Video",
+      "YouTube Short",
+      "YouTube Title",
+      "YouTube Description",
+      "YouTube Tag",
+      "YouTube Thumbnail",
+    ],
+  },
+  {
+    name: "Twitter",
+    icon: "/platformIcons/twitterHQ.png",
+    color: "#1DA1F2",
+    items: ["Twitter Tweet", "Twitter Hashtag", "Twitter Bio"],
+  },
+  {
+    name: "Instagram",
+    icon: "/platformIcons/instagramHiQi.png",
+    color: "#C13584",
+    items: [
+      "Instagram Reel",
+      "Instagram Caption",
+      "Instagram Bio",
+      "Instagram Hashtag",
+    ],
+  },
+  {
+    name: "LinkedIn",
+    icon: "/platformIcons/linkednHQ.png",
+    color: "#0077B5",
+    items: [
+      "LinkedIn Post",
+      "LinkedIn Profile Optimization",
+      "LinkedIn Articles",
+    ],
+  },
+
+  {
+    name: "Facebook",
+    icon: "/platformIcons/facebookHiQ.png",
+    color: "#4267B2",
+    items: ["Facebook Post", "Facebook Ad Copy", "Facebook Page Description"],
+  },
+  {
+    name: "TikTok",
+    icon: "/platformIcons/tiktokHQ.png",
+    color: "#FE2C55",
+    items: ["TikTok Video", "TikTok Hashtag", "TikTok Caption"],
+  },
+  // {
+  //   name: "General",
+  //   icon: '/platformIcons/general.png',
+  //   color: "#FE2C55",
+  //   items: ["Rewrite", "Repurpose"]
+  // }
+];
+
+export default platforms;
