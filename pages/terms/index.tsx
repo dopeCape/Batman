ab4397ca31880b6a1276a96525b948465a411ede
Mr. Robot <gomexxxx71@gmail.com>
2023-09-08 22:15:33 +0600

    build terms of service page

---
+++ pages/terms/index.tsx
@@ -0,0 +1,288 @@
+const Terms = () => {
+  return (
+    <div className="px-[7%]">
+      <h1 className="font-semibold text-6xl leading-[92%] text-black mt-4">
+        Terms of Service
+      </h1>
+      <h2 className="font-semibold text-4xl leading-[92%] text-black mt-4">
+        Introduction
+      </h2>
+      <p>
+        Following are the Terms of Service for Metridash's services (“Service,”
+        “Metridash,” “we,” or “us”). These terms and conditions govern your use
+        of this website (“website”); by using this website, you accept these
+        terms and conditions in full. If you disagree with these terms and
+        conditions or any part of these terms and conditions, you must not use
+        this website. This website uses cookies. By using this website and
+        agreeing to these terms and conditions, you consent to our Metridash's
+        use of cookies in accordance with the terms of Metridash's privacy
+        policy.
+      </p>
+      <h2 className="font-semibold text-4xl leading-[92%] text-black mt-4">
+        License to use website
+      </h2>
+      <p>
+        Unless otherwise stated, Metridash and/or its licensors own the
+        intellectual property rights in the website and material on the website.
+        Subject to the license below, all these intellectual property rights are
+        reserved. Metridash hereby grants you a non-exclusive, non-transferable
+        subscription to use the Service during the Term, solely for your
+        internal use in accordance with the use parameters described in the
+        order form utilized to order such subscription and subject to these
+        Terms of Service. By using Metridash to collect data from and optimize
+        your social media channels, you're also agreeing to be bound by their
+        Terms of Service. YouTube's Terms of Service can be found{" "}
+        <a href="https://www.youtube.com/t/terms">here.</a>
+      </p>
+      <h2 className="font-semibold text-4xl leading-[92%] text-black mt-4">
+        Setting up your account
+      </h2>
+      <ul className="list-disc">
+        <li>
+          You must provide accurate and complete information, including your
+          legal full name, a working email address, and any other information
+          requested during the account signup process to obtain an account and
+          use the Service, and update this information if it changes.
+        </li>
+        <li>
+          You must be a human being to set up an account and use the Service.
+          Accounts may not be created by “bots” or other automated methods.
+        </li>
+        <li>
+          You are responsible for keeping your account and password secure, and
+          are also responsible for all activities using your account or
+          password. Metridash is not liable for any loss or damage that results
+          from your failure to comply with this obligation or unauthorized use
+          of your account.
+        </li>
+        <li>You may never use another’s account without permission.</li>
+        <li>
+          You must notify Metridash immediately of any breach of security or
+          unauthorized use of your account.
+        </li>
+      </ul>
+      <h2 className="font-semibold text-4xl leading-[92%] text-black mt-4">
+        Working Together
+      </h2>
+      <p>
+        We'll be a trusted partner that works closely and collaboratively with
+        you and your team to reach your marketing and growth goals. We also like
+        to celebrate our customers' successes! If we ask, you'll allow us to
+        create a case study highlighting your wins and how Metridash has helped
+        achieve them.
+      </p>
+      <h2 className="font-semibold text-4xl leading-[92%] text-black mt-4">
+        Fees and payment
+      </h2>
+      <p>
+        Payment and pricing terms for the Service are as specified in the order
+        form utilized to order such subscriptions. By selecting a premium
+        service you agree to pay Metridash the monthly or annual subscription
+        fees indicated for that service. Payments will be charged on the day you
+        sign up for a premium service and will cover the use of that service for
+        a monthly or annual period as indicated. Premium service fees are not
+        refundable. Metridash offers monthly and annual subscriptions that renew
+        until they are canceled by the user. Your subscription will
+        automatically renew for another period unless notice of non-renewal is
+        provided. Your notice of non-renewal must be received before the renewal
+        period begins. You will be billed for the Service until you properly
+        cancel your account or Metridash terminates your account. Your
+        cancellation may not take effect until completion of the current billing
+        cycle. Prices for the Service are subject to change upon 30 days notice
+        from Metridash. This notice may be posted on the Metridash website or
+        may appear with the Service itself. If you do not agree to the price
+        change(s), you may cancel your account during this 30-day period. By
+        continuing to use the Service after the effective date of a pricing
+        change, you thereby agree to such pricing change. Metridash shall not be
+        liable to you or to any third party for any modifications to the Service
+        or prices.
+      </p>
+      <h2 className="font-semibold text-4xl leading-[92%] text-black mt-4">
+        Cancellation and Termination
+      </h2>
+      <p>
+        {" "}
+        You are solely responsible for properly closing your account. You can
+        close your account at any time by emailing us at info@metridash.com.
+        Payments are made for the upcoming billing cycle. Metridash does not
+        provide prorated refunds for accounts that are canceled during their
+        subscription term.
+      </p>
+      <h2 className="font-semibold text-4xl leading-[92%] text-black mt-4">
+        Acceptable use
+      </h2>
+      <p>
+        You must not use this website in any way that causes, or may cause,
+        damage to the website or impairment of the availability or accessibility
+        of the website; or in any way which is unlawful, illegal, fraudulent or
+        harmful, or in connection with any unlawful, illegal, fraudulent or
+        harmful purpose or activity. You must not use this website to copy,
+        store, host, transmit, send, use, publish or distribute any material
+        which consists of (or is linked to) any spyware, computer virus, Trojan
+        horse, worm, keystroke logger, rootkit or other malicious computer
+        software. You must not conduct any systematic or automated data
+        collection activities (including without limitation scraping, data
+        mining, data extraction and data harvesting) on or in relation to this
+        website without Metridash's express written consent. You must not use
+        this website to transmit or send unsolicited commercial communications.
+      </p>
+      <h2 className="font-semibold text-4xl leading-[92%] text-black mt-4">
+        Restricted Access
+      </h2>
+      <p>
+        Access to certain areas of this website is restricted. Metridash
+        reserves the right to restrict access to areas of this website, or
+        indeed this entire website, at Metridash's discretion. If Metridash
+        provides you with a user ID and password to enable you to access
+        restricted areas of this website or other content or services, you must
+        ensure that the user ID and password are kept confidential. Metridash
+        may disable your user ID and password in Metridash's sole discretion
+        without notice or explanation.
+      </p>
+      <h2 className="font-semibold text-4xl leading-[92%] text-black mt-4">
+        Limitations of liability
+      </h2>
+      <p>
+        Metridash will not be liable to you (whether under the law of contact,
+        the law of torts or otherwise) in relation to the contents of, or use
+        of, or otherwise in connection with, this website:
+      </p>
+      <ul className="list-disc">
+        <li>for any direct loss;</li>
+        <li>for any indirect, special or consequential loss; or</li>
+        <li>
+          for any business losses, loss of revenue, income, profits or
+          anticipated savings, loss of contracts or business relationships, loss
+          of reputation or goodwill, or loss or corruption of information or
+          data.
+        </li>
+      </ul>
+      <p>
+        These limitations of liability apply even if Metridash has been
+        expressly advised of the potential loss.
+      </p>
+      <h2 className="font-semibold text-4xl leading-[92%] text-black mt-4">
+        Exceptions
+      </h2>
+      <p>
+        Nothing in this website disclaimer will exclude or limit any warranty
+        implied by law that it would be unlawful to exclude or limit; and
+        nothing in this website disclaimer will exclude or limit Metridash's
+        liability in respect of any:
+      </p>
+      <ul className="list-disc">
+        <li>death or personal injury caused by Metridash's negligence;</li>
+        <li>
+          fraud or fraudulent misrepresentation on the part of Metridash; or
+        </li>
+        <li>
+          matter which it would be illegal or unlawful for Metridash to exclude
+          or limit, or to attempt or purport to exclude or limit, its liability.
+        </li>
+      </ul>
+      <h2 className="font-semibold text-4xl leading-[92%] text-black mt-4">
+        Reasonableness
+      </h2>
+      <p>
+        By using this website, you agree that the exclusions and limitations of
+        liability set out in this website disclaimer are reasonable. If you do
+        not think they are reasonable, you must not use this website.
+      </p>
+      <h2 className="font-semibold text-4xl leading-[92%] text-black mt-4">
+        Other parties
+      </h2>
+      <p>
+        You accept that, as a limited liability entity, Metridash has an
+        interest in limiting the personal liability of its officers and
+        employees. You agree that you will not bring any claim personally
+        against Metridash's officers or employees in respect of any losses you
+        suffer in connection with the website. You agree that the limitations of
+        warranties and liability set out in this website disclaimer will protect
+        Metridash's officers, employees, agents, subsidiaries, successors,
+        assigns and sub-contractors as well as Metridash.
+      </p>
+      <h2 className="font-semibold text-4xl leading-[92%] text-black mt-4">
+        Unenforceable provisions
+      </h2>
+      <p>
+        If any provision of this website disclaimer is, or is found to be,
+        unenforceable under applicable law, that will not affect the
+        enforceability of the other provisions of this website disclaimer.
+      </p>
+      <h2 className="font-semibold text-4xl leading-[92%] text-black mt-4">
+        Indemnity
+      </h2>
+      <p>
+        You hereby indemnify Metridash and undertake to keep Metridash
+        indemnified against any losses, damages, costs, liabilities and expenses
+        (including without limitation legal expenses and any amounts paid by
+        Metridash to a third party in settlement of a claim or dispute on the
+        advice of Metridash's legal advisers) incurred or suffered by Metridash
+        arising out of any breach by you of any provision of these terms and
+        conditions, or arising out of any claim that you have breached any
+        provision of these terms and conditions.
+      </p>
+      <h2 className="font-semibold text-4xl leading-[92%] text-black mt-4">
+        Breaches of these terms and conditions
+      </h2>
+      <p>
+        Without prejudice to Metridash's other rights under these terms and
+        conditions, if you breach these terms and conditions in any way,
+        Metridash may take such action as Metridash deems appropriate to deal
+        with the breach, including suspending your access to the website,
+        prohibiting you from accessing the website, blocking computers using
+        your IP address from accessing the website, contacting your internet
+        service provider to request that they block your access to the website
+        and/or bringing court proceedings against you.
+      </p>
+      <h2 className="font-semibold text-4xl leading-[92%] text-black mt-4">
+        Variation
+      </h2>
+      <p>
+        Metridash may revise these terms and conditions from time-to-time.
+        Revised terms and conditions will apply to the use of this website from
+        the date of the publication of the revised terms and conditions on this
+        website. Please check this page regularly to ensure you are familiar
+        with the current version.
+      </p>
+      <h2 className="font-semibold text-4xl leading-[92%] text-black mt-4">
+        Assignment
+      </h2>
+      <p>
+        Metridash may transfer, sub-contract or otherwise deal with Metridash's
+        rights and/or obligations under these terms and conditions without
+        notifying you or obtaining your consent. You may not transfer,
+        sub-contract or otherwise deal with your rights and/or obligations under
+        these terms and conditions.
+      </p>
+      <h2 className="font-semibold text-4xl leading-[92%] text-black mt-4">
+        Severability
+      </h2>
+      <p>
+        If a provision of these terms and conditions is determined by any court
+        or other competent authority to be unlawful and/or unenforceable, the
+        other provisions will continue in effect. If any unlawful and/or
+        unenforceable provision would be lawful or enforceable if part of it
+        were deleted, that part will be deemed to be deleted, and the rest of
+        the provision will continue in effect.
+      </p>
+      <h2 className="font-semibold text-4xl leading-[92%] text-black mt-4">
+        Entire Agreement
+      </h2>
+      <p>
+        These terms and conditions constitute the entire agreement between you
+        and Metridash in relation to your use of this website, and supersede all
+        previous agreements in respect of your use of this website.
+      </p>
+      <h2 className="font-semibold text-4xl leading-[92%] text-black mt-4">
+        Metridash's details
+      </h2>
+      <p>
+        Metridash is a service offered by ElsaRaelyn Pvt Ltd. You can contact
+        Metridash by email at info@metridash.com
+      </p>
+    </div>
+  )
+}
+
+export default Terms
