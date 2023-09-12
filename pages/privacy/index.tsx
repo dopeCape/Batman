592e82f27aac9a9ccc7c015f7fba3d1075a08be4
Mr. Robot <gomexxxx71@gmail.com>
2023-09-12 20:21:05 +0600

    build privacy policy

---
+++ pages/privacy/index.tsx
@@ -0,0 +1,273 @@
+const Privacy = () => {
+  return (
+    <div className="px-[7%]">
+      <h1 className="font-semibold text-6xl leading-[92%] w-[408px] text-black mt-4">
+        Privacy Policy
+      </h1>
+      <p className="py-5">
+        This Privacy Policy governs the manner in which ElsaRaelyn Pvt Ltd.
+        d/b/a Metridash (“Metridash”) collects, uses, maintains and discloses
+        information collected from users (each, a “User”) of the
+        https://metridash.com website (“Site”), and individuals who are
+        third-party content authors of social media content (“Social Media
+        Authors”). This privacy policy applies to the Site and all products and
+        services offered by Metridash Pvt Ltd.
+      </p>
+      <h2 className="font-semibold text-4xl leading-[92%] text-black mt-4">
+        Personal identification information
+      </h2>
+      <h3 className="text-2xl py-5">Users</h3>
+      <ul className="list-disc">
+        <li>
+          We may collect personal identification information from Users in a
+          variety of ways, including, but not limited to, when Users visit our
+          site, register on the site, place an order, subscribe to the
+          newsletter, and in connection with other activities, services,
+          features or resources we make available on our Site.
+        </li>
+        <li>
+          Users may be asked for, as appropriate, name, email address, phone
+          number, credit card information.
+        </li>
+        <li>
+          Users may, however, visit our Site anonymously. We will collect
+          personal identification information from Users only if they
+          voluntarily submit such information to us.
+        </li>
+        <li>
+          Users can always refuse to supply personal identification information,
+          except that it may prevent them from engaging in certain Site-related
+          activities.
+        </li>
+      </ul>
+      <h3>Social Media Authors</h3>
+      <p className="py-5">
+        Metridash receives personal data from third parties such as Facebook,
+        Twitter, Instagram, and YouTube for our legitimate interests of serving
+        our customers. We are fully compliant with their public terms and
+        conditions and trust them to protect Social Media Authors through their
+        robust privacy policies.
+      </p>
+      <ul className="list-disc">
+        <li>
+          Metridash collects, uses, processes and stores information that has
+          been made publicly available on the Social Media platforms listed
+          above such as: 1. names, usernames, handles, or other identifiers; 2.
+          the content of the information published via that name, username,
+          handle, or other identifier, including comments, expressions,
+          opinions, posts, etc.; 3. profile pictures or other images or videos
+          posted or interacted with; 4. publicly disclosed location; 5. gender;
+          6. We may also use this information to infer other data, such as the
+          sentiment of the post.
+        </li>
+        <li>
+          To collect & provide customers with data about their YouTube channels,
+          Metridash uses{" "}
+          <a href="https://developers.google.com/youtube/terms/developer-policies#definition-youtube-api-services">
+            YouTube API Service.
+          </a>{" "}
+          Google Privacy Policy can be found{" "}
+          <a href="https://policies.google.com/privacy">here.</a>
+          1. In addition to our normal procedure for deleting stored data, you
+          can revoke Metridash's access to your YouTube data via the{" "}
+          <a href="https://security.google.com/settings/security/permissions">
+            Google security setting page
+          </a>{" "}
+          .
+        </li>
+      </ul>
+      <h2 className="font-semibold text-4xl leading-[92%] text-black mt-4">
+        Rights of Users and Social Media Authors
+      </h2>
+      <p>
+        At any time, you may contact info@metridash.com and request that we:
+      </p>
+      <ul className="list-disc">
+        <li>Rectify personal data Metridash has collected</li>
+        <li>Provide you with a copy of your personal data</li>
+        <li>
+          Permanently delete any personal or non-personal data (such as cookies)
+          that we have collected and shared
+        </li>
+        <li>Refuse any use which relates to marketing</li>
+      </ul>
+      <h2 className="font-semibold text-4xl leading-[92%] text-black mt-4">
+        Non-personal identification information
+      </h2>
+      <p>
+        We may collect non-personal identification information about Users
+        whenever they interact with our Site. Non-personal identification
+        information may include the browser name, the type of computer and
+        technical information about Users’ means of connection to our Site, such
+        as the operating system and the Internet service providers utilized and
+        other similar information.
+      </p>
+      <h2 className="font-semibold text-4xl leading-[92%] text-black mt-4">
+        Web browser cookies
+      </h2>
+      <p>
+        Our Site may use “cookies” to enhance User experience. Users’ web
+        browsers place cookies on their hard drive for record-keeping purposes
+        and sometimes to track information about them. The User may choose to
+        set their web browser to refuse cookies, or to alert you when cookies
+        are being sent. If they do so, note that some parts of the Site may not
+        function properly. As you browse Metridash, advertising cookies will be
+        placed on your computer so that we can understand what you are
+        interested in. Our advertising partner then enables us to present you
+        with retargeting advertising on other sites based on your previous
+        interaction with Metridash. The techniques our partners employ do not
+        collect personal information such as your name, email address, postal
+        address or telephone number. You can visit
+        http://www.networkadvertising.org/choices to opt out of our partners'
+        targeted advertising. It is your right to contact info@metridash.com and
+        request that we delete any cookies that link back to you and your online
+        activity.
+      </p>
+      <h2 className="font-semibold text-4xl leading-[92%] text-black mt-4">
+        How we use collected information
+      </h2>
+      <h2 className="font-semibold py-5">Users</h2>
+      <p>
+        Metridash may collect and use Users personal information for the
+        following purposes:
+      </p>
+      <ul className="list-disc">
+        <li>
+          To improve customer service. Information you provide helps us respond
+          to your customer service requests and support needs more efficiently.
+        </li>
+        <li>
+          To personalize user experience. We may use information in the
+          aggregate to understand how our Users as a group use the services and
+          resources provided on our Site.
+        </li>
+        <li>
+          To improve our Site. We may use feedback you provide to improve our
+          products and services.
+        </li>
+        <li>
+          To process payments. We may use the information Users provide about
+          themselves when placing an order only to provide service to that
+          order. We do not share this information with outside parties except to
+          the extent necessary to provide the service.
+        </li>
+        <li>To run a promotion, contest, survey or other Site feature.</li>
+        <li>
+          To send Users information they agreed to receive about topics we think
+          will be of interest to them.
+        </li>
+        <li>To send periodic emails.</li>
+        <li>
+          We may use the email address to send User information and updates
+          pertaining to their order.
+        </li>
+        <li>
+          It may also be used to respond to their inquiries, questions, and/or
+          other requests.
+        </li>
+        <li>
+          If User decides to opt-in to our mailing list, they will receive
+          emails that may include company news, updates, related product or
+          service information, etc. If at any time the User would like to
+          unsubscribe from receiving future emails, we include detailed
+          unsubscribe instructions at the bottom of each email.
+        </li>
+      </ul>
+      <h2>Social Media Authors</h2>
+      <p>
+        The legal basis for the data that we collect and process is pursuant to
+        our legitimate interests in providing our Services to our customers.
+      </p>
+      <p>Your information is also used in the following ways:</p>
+      <ul>
+        <li>
+          To allow our customers to learn more about their brand, customers, and
+          competitors;
+        </li>
+        <li>To improve our Services.</li>
+      </ul>
+      <h2 className="font-semibold text-4xl leading-[92%] text-black mt-4">
+        How we protect your information
+      </h2>
+      <p>
+        We adopt appropriate data collection, storage and processing practices
+        and security measures to protect against unauthorized access,
+        alteration, disclosure or destruction of your personal information,
+        username, password, transaction information and data stored on our Site.
+      </p>
+      <p>
+        In compliance with our company’s values and GDPR laws, you will always
+        be immediately notified in the unlikely event of data breach.
+      </p>
+      <h2 className="font-semibold text-4xl leading-[92%] text-black mt-4">
+        Sharing your personal information
+      </h2>
+      <p>
+        We do not sell, trade, or rent Users’ personal identification
+        information to others. We may share generic aggregated demographic
+        information not linked to any personal identification information
+        regarding visitors and users with our business partners, trusted
+        affiliates and advertisers for the purposes outlined above. We may use
+        third-party service providers to help us operate our business and the
+        Site or administer activities on our behalf, such as sending out
+        newsletters or surveys. We may share your information with these third
+        parties for those limited purposes provided that you have given us your
+        permission.
+      </p>
+      <h2>Social Media Authors</h2>
+      <p>
+        We share your data with our Customers in the ways outlined in Personal
+        identification information.
+      </p>
+      <h2 className="font-semibold text-4xl leading-[92%] text-black mt-4">
+        Third party websites
+      </h2>
+      <p>
+        Users may find advertising or other content on our Site that link to the
+        sites and services of our partners, suppliers, advertisers, sponsors,
+        licensors and other third parties. We do not control the content or
+        links that appear on these sites and are not responsible for the
+        practices employed by websites linked to or from our Site. In addition,
+        these sites or services, including their content and links, may be
+        constantly changing. These sites and services may have their own privacy
+        policies and customer service policies. Browsing and interaction on any
+        other website, including websites which have a link to our Site, is
+        subject to that website's own terms and policies.
+      </p>
+      <h2 className="font-semibold text-4xl leading-[92%] text-black mt-4">
+        Changes to this privacy policy
+      </h2>
+      <p>
+        Metridash has the discretion to update this privacy policy at any time.
+        When we do, we will revise the updated date at the bottom of this page.
+        We encourage Users to frequently check this page for any changes to stay
+        informed about how we are helping to protect the personal information we
+        collect. You acknowledge and agree that it is your responsibility to
+        review this privacy policy periodically and become aware of
+        modifications.
+      </p>
+      <h2 className="font-semibold text-4xl leading-[92%] text-black mt-4">
+        Your acceptance of these terms
+      </h2>
+      <p>
+        By using this Site, you signify your acceptance of this policy. If you
+        do not agree to this policy, please do not use our Site. Your continued
+        use of the Site following the posting of changes to this policy will be
+        deemed your acceptance of those changes.
+      </p>
+      <h2 className="font-semibold text-4xl leading-[92%] text-black mt-4">
+        Contacting Us
+      </h2>
+      <p>
+        If you have any questions about this Privacy Policy, the practices of
+        this site, or your dealings with this site, please contact us at:
+        Metridash Pvt Ltd. <br /> https://metridash.com/ <br /> 181 Nethaji
+        Road, Kokkirakulam <br /> Tirunelveli, Tamil Nadu <br /> India <br />{" "}
+        +91 9994412727 <br /> info@metridash.com <br />
+        This document was last updated on Sep 7th, 2023
+      </p>
+    </div>
+  )
+}
+
+export default Privacy
