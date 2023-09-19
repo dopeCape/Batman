278cda38d53f782f3cbd4369ffcf195f561788ad
Mr. Robot <gomexxxx71@gmail.com>
2023-09-19 15:34:51 +0600

    fix token

--- auth.js
+++ auth.js
@@ -76,10 +76,10 @@ export const generateRealTimeToken = (user) => {
 export const getUserToken = async (user) => {
   if (!user) return null
 
-  const tokenRef = doc(firestore, "users", user.uid)
+  const tokenRef = doc(db, "users", user.uid)
   const doc1 = await getDoc(tokenRef)
 
-  if (doc1.exists) {
+  if (doc1.exists()) {
     return doc1.data().tokens
   } else {
     return null
@@ -97,18 +97,15 @@ export const updateModel = async (user, newModelValue) => {
   await updateDoc(userRef, { model: newModelValue })
 }
 
-export const addDraft = async (user, data, platform) => {
+export const addDraft = async (user, data) => {
   const userRef = doc(db, "users", user.uid)
-  const newObject = {draft:data, platform: user}
-  const CurrentDate = new Date();
-  
- 
+
   try {
     const userDoc = await getDoc(userRef)
 
     if (userDoc.exists()) {
       await updateDoc(userRef, {
-        draft: arrayUnion({draft:data, platform: platform, date: CurrentDate}),
+        draft: arrayUnion(data),
       })
       ;<Alert severity="success">This is a success alert — check it out!</Alert>
     } else {
@@ -116,7 +113,6 @@ export const addDraft = async (user, data, platform) => {
     }
   } catch (error) {
     alert("Error:", error)
-    console.log(error)
   }
 }
 
@@ -138,48 +134,19 @@ export const fetchUserDrafts = async (user) => {
   }
 }
 
-// export const createUserWithEmail = async (email, password) => {
-//   const { user } = await createUserWithEmailAndPassword( auth, email, password)
-//   const userData = {
-//     email: email,
-//     tokens: 100,
-//     model: "text-davinci-002",
-//     isNewUser: true,
-//     uid: user.uid,
-//     drafts: [],
-//   }
- 
-//   const data = await setDoc(doc(firestore, "users", user.uid), userData)
-//   return user, data
-// }
-
 export const createUserWithEmail = async (email, password) => {
-  try {
-    const { user } = await createUserWithEmailAndPassword(auth, email, password);
-
-    const userData = {
-      email: email,
-      tokens: 100,
-      model: "text-davinci-002",
-      isNewUser: true,
-      uid: user.uid,
-      drafts: [],
-    };
-
-    const userDocRef = doc(firestore, "users", user.uid);
-    await setDoc(userDocRef, userData);
-
-    return { user, data: "Data successfully saved" }; // Return user and a success message
-  } catch (error) {
-    console.error("Error creating user:", error.message);
-    throw error; // Rethrow the error to handle it further if needed
+  const { user } = await createUserWithEmailAndPassword(auth, email, password)
+  const userData = {
+    email: user.email,
+    tokens: 100,
+    model: "text-davinci-002",
+    isNewUser: true,
+    uid: user.uid,
+    drafts: [],
   }
-};
-
-
-
-
-
+  await setDoc(doc(firestore, "users", user.uid), userData)
+  return user
+}
 
 export const signInWithEmail = async (email, password) => {
   try {
@@ -448,3 +415,448 @@ export const onUserSignedIn = async (user, router) => {
 //     router.push("/");
 //   }
 // };
+
+// import {
+//   getAuth,
+//   createUserWithEmailAndPassword,
+//   signInWithEmailAndPassword,
+//   signInWithPopup,
+//   GoogleAuthProvider,
+//   signOut,
+// } from "firebase/auth"
+// import {
+//   getFirestore,
+//   doc,
+//   setDoc,
+//   getDoc,
+//   getDocs,
+//   onSnapshot,
+//   collection,
+//   query,
+//   where,
+//   arrayUnion,
+// } from "firebase/firestore"
+// import { db } from "./firebase"
+// import { updateDoc } from "firebase/firestore"
+// import Alert from "@mui/material/Alert"
+
+// // import { db } from './firebaseConfig';
+// import { app } from "./firebaseConfig"
+// const auth = getAuth(app)
+
+// const firestore = getFirestore(app)
+
+// const googleProvider = new GoogleAuthProvider()
+
+// export const readTokens = async (user) => {
+//   const userRef = doc(firestore, "users", user.uid)
+//   const userDoc = await userRef.get()
+//   const userData = userDoc.data()
+//   if (userData) {
+//     return userData.tokens
+//   }
+//   return null
+// }
+
+// export const getRealTimeToken = async (user) => {
+//   let value
+//   const tokenRef = doc(firestore, "users", user.uid)
+//   await onSnapshot(tokenRef, (snapshot) => {
+//     value = snapshot.data().tokens
+//   })
+//   return value
+
+//   // if (doc1.exists) {
+//   //   console.log("this is real time data "+doc1.data().tokens)
+//   //   return doc1
+//   // } else {
+//   //   return null;
+//   // }
+// }
+
+// export const generateRealTimeToken = (user) => {
+//   // Get the current user's token
+//   if (!user) return null
+//   let token = getUserToken(user)
+//   const tokenRef = doc(firestore, "users", user.uid)
+//   // Listen for changes to the token
+//   onSnapshot(tokenRef, (newToken) => {
+//     // Update the token if it has changed
+//     if (newToken !== token) {
+//       token = newToken
+//     }
+//   })
+
+//   // Return the current token
+//   return token
+// }
+
+// export const getUserToken = async (user) => {
+//   if (!user) return null
+
+//   const tokenRef = doc(firestore, "users", user.uid)
+//   const doc1 = await getDoc(tokenRef)
+
+//   if (doc1.exists) {
+//     return doc1.data().tokens
+//   } else {
+//     return null
+//   }
+// }
+
+// export const updateTokens = async (user, newTokenValue) => {
+//   const userRef = doc(firestore, "users", user.uid)
+
+//   await updateDoc(userRef, { tokens: newTokenValue })
+// }
+
+// export const updateModel = async (user, newModelValue) => {
+//   const userRef = doc(firestore, "users", user.uid)
+//   await updateDoc(userRef, { model: newModelValue })
+// }
+
+// export const addDraft = async (user, data, platform) => {
+//   const userRef = doc(db, "users", user.uid)
+//   const newObject = {draft:data, platform: user}
+//   const CurrentDate = new Date();
+
+//   try {
+//     const userDoc = await getDoc(userRef)
+
+//     if (userDoc.exists()) {
+//       await updateDoc(userRef, {
+//         draft: arrayUnion({draft:data, platform: platform, date: CurrentDate}),
+//       })
+//       ;<Alert severity="success">This is a success alert — check it out!</Alert>
+//     } else {
+//       alert("User document not found")
+//     }
+//   } catch (error) {
+//     alert("Error:", error)
+//     console.log(error)
+//   }
+// }
+
+// export const fetchUserDrafts = async (user) => {
+//   const userRef = doc(db, "users", user.uid)
+
+//   try {
+//     const docSnap = await getDoc(userRef)
+
+//     if (docSnap.exists()) {
+//       const userData = docSnap.data()
+//       const userDrafts = userData.draft || []
+//       return userDrafts
+//     } else {
+//       throw new Error("User document not found")
+//     }
+//   } catch (error) {
+//     throw new Error("Error fetching drafts: " + error.message)
+//   }
+// }
+
+// // export const createUserWithEmail = async (email, password) => {
+// //   const { user } = await createUserWithEmailAndPassword( auth, email, password)
+// //   const userData = {
+// //     email: email,
+// //     tokens: 100,
+// //     model: "text-davinci-002",
+// //     isNewUser: true,
+// //     uid: user.uid,
+// //     drafts: [],
+// //   }
+
+// //   const data = await setDoc(doc(firestore, "users", user.uid), userData)
+// //   return user, data
+// // }
+
+// export const createUserWithEmail = async (email, password) => {
+//   try {
+//     const { user } = await createUserWithEmailAndPassword(auth, email, password);
+
+//     const userData = {
+//       email: email,
+//       tokens: 100,
+//       model: "text-davinci-002",
+//       isNewUser: true,
+//       uid: user.uid,
+//       drafts: [],
+//     };
+
+//     const userDocRef = doc(firestore, "users", user.uid);
+//     await setDoc(userDocRef, userData);
+
+//     return { user, data: "Data successfully saved" }; // Return user and a success message
+//   } catch (error) {
+//     console.error("Error creating user:", error.message);
+//     throw error; // Rethrow the error to handle it further if needed
+//   }
+// };
+
+// export const signInWithEmail = async (email, password) => {
+//   try {
+//     const { user } = await signInWithEmailAndPassword(auth, email, password)
+//     return user
+//   } catch (error) {
+//     if (error.code === "auth/user-not-found") {
+//       throw new Error("User does not exist")
+//     } else {
+//       throw error
+//     }
+//   }
+// }
+
+// export const Logout = async () => {
+//   try {
+//     await signOut(auth)
+//     localStorage.removeItem("token")
+//   } catch (err) {
+//     console.error(err)
+//   }
+// }
+
+// export const signInWithGoogle = async () => {
+//   const googleProvider = new GoogleAuthProvider()
+//   const { user } = await signInWithPopup(auth, googleProvider)
+
+//   const userRef = collection(db, "users")
+//   const q = query(userRef, where("email", "==", user.email))
+
+//   const querySnapshot = await getDocs(q)
+//   let checkUser = []
+//   querySnapshot.forEach((doc) => {
+//     checkUser.push({ id: doc.id, ...doc.data() })
+//   })
+
+//   if (checkUser.length > 0) return
+
+//   const userData = {
+//     email: user.email,
+//     tokens: 100,
+//     model: "text-davinci-002",
+//     isNewUser: true,
+//     uid: user.uid,
+//   }
+//   await setDoc(doc(firestore, "users", user.uid), userData, { merge: true })
+
+//   return user
+// }
+
+// export const onUserSignedIn = async (user, router) => {
+//   const userDoc = await doc(firestore, "users", user.uid).get()
+//   const isNewUser = userDoc.data().isNewUser
+//   if (isNewUser) {
+//     router.push("/")
+//   } else {
+//     router.push("/")
+//   }
+// }
+
+// // import {
+// //   getAuth,
+// //   createUserWithEmailAndPassword,
+// //   signInWithEmailAndPassword,
+// //   signInWithPopup,
+// //   GoogleAuthProvider,
+// //   signOut,
+// // } from "firebase/auth";
+// // import {
+// //   getFirestore,
+// //   doc,
+// //   setDoc,
+// //   getDoc,
+// //   getDocs,
+// //   onSnapshot,
+// //   collection,
+// //   query,
+// //   where,
+// //   arrayUnion
+// // } from "firebase/firestore";
+// // import { db } from "./firebase";
+// // import { updateDoc } from "firebase/firestore";
+// // import Alert from '@mui/material/Alert';
+
+// // // import { db } from './firebaseConfig';
+// // import { app } from "./firebaseConfig";
+// // const auth = getAuth(app);
+
+// // const firestore = getFirestore(app);
+
+// // const googleProvider = new GoogleAuthProvider();
+
+// // export const readTokens = async (user) => {
+// //   const userRef = doc(firestore, "users", user.uid);
+// //   const userDoc = await userRef.get();
+// //   const userData = userDoc.data();
+// //   if (userData) {
+// //     return userData.tokens;
+// //   }
+// //   return null;
+// // };
+
+// // export const getRealTimeToken = async (user) => {
+// //   let value;
+// //   const tokenRef = doc(firestore, "users", user.uid);
+// //   await onSnapshot(tokenRef, (snapshot) => {
+// //     value = snapshot.data().tokens;
+// //   });
+// //   return value;
+
+// //   // if (doc1.exists) {
+// //   //   console.log("this is real time data "+doc1.data().tokens)
+// //   //   return doc1
+// //   // } else {
+// //   //   return null;
+// //   // }
+// // };
+
+// // export const generateRealTimeToken = (user) => {
+// //   // Get the current user's token
+// //   if (!user) return null;
+// //   let token = getUserToken(user);
+// //   const tokenRef = doc(firestore, "users", user.uid);
+// //   // Listen for changes to the token
+// //   onSnapshot(tokenRef, (newToken) => {
+// //     // Update the token if it has changed
+// //     if (newToken !== token) {
+// //       token = newToken;
+// //     }
+// //   });
+
+// //   // Return the current token
+// //   return token;
+// // };
+
+// // export const getUserToken = async (user) => {
+// //   if (!user) return null;
+
+// //   const tokenRef = doc(firestore, "users", user.uid);
+// //   const doc1 = await getDoc(tokenRef);
+
+// //   if (doc1.exists) {
+// //     return doc1.data().tokens;
+// //   } else {
+// //     return null;
+// //   }
+// // };
+
+// // export const updateTokens = async (user, newTokenValue) => {
+// //   const userRef = doc(firestore, "users", user.uid);
+
+// //   await updateDoc(userRef, { tokens: newTokenValue });
+// // };
+
+// // export const updateModel = async (user, newModelValue) => {
+// //   const userRef = doc(firestore, "users", user.uid);
+// //   await updateDoc(userRef, { model: newModelValue });
+// // };
+
+// // export const addDraft = async (user, data) => {
+// //   const userRef = doc(firestore, 'users', user.uid);
+
+// //   try {
+// //     const userDoc = await getDoc(userRef);
+
+// //     if (userDoc.exists()) {
+
+// //       await updateDoc(userRef, {
+// //         draft: arrayUnion(data)
+// //       });
+
+// //       <Alert severity="success">This is a success alert — check it out!</Alert>
+// //     } else {
+// //       alert('User document not found');
+// //     }
+// //   } catch (error) {
+// //     alert('Error:', error);
+// //   }
+// // };
+
+// // export const fetchUserDrafts = async (user) => {
+// //   const userRef = doc(firestore, 'users', user.uid);
+
+// //   try {
+// //     const docSnap = await getDoc(userRef);
+
+// //     if (docSnap.exists()) {
+// //       const userData = docSnap.data();
+// //       const userDrafts = userData.draft || [];
+// //       return userDrafts;
+// //     } else {
+// //       throw new Error('User document not found');
+// //     }
+// //   } catch (error) {
+// //     throw new Error('Error fetching drafts: ' + error.message);
+// //   }
+// // };
+
+// // export const createUserWithEmail = async (email, password) => {
+// //   const { user } = await createUserWithEmailAndPassword(auth, email, password);
+// //   const userData = {
+// //     email: user.email,
+// //     tokens: 100,
+// //     model: "text-davinci-002",
+// //     isNewUser: true,
+// //     uid: user.uid,
+// //     drafts: []
+// //   };
+// //   await setDoc(doc(firestore, "users", user.uid), userData);
+// //   return user;
+// // };
+
+// // export const signInWithEmail = async (email, password) => {
+// //   try {
+// //     const { user } = await signInWithEmailAndPassword(auth, email, password);
+// //     return user;
+// //   } catch (error) {
+// //     if (error.code === "auth/user-not-found") {
+// //       throw new Error("User does not exist");
+// //     } else {
+// //       throw error;
+// //     }
+// //   }
+// // };
+
+// // export const Logout = async () => {
+// //   try {
+// //     await signOut(auth);
+// //   } catch (err) {
+// //     console.error(err);
+// //   }
+// // };
+
+// // export const signInWithGoogle = async () => {
+// //   const googleProvider = new GoogleAuthProvider();
+// //   const { user } = await signInWithPopup(auth, googleProvider);
+
+// //   const userRef = collection(db, "users");
+// //   const q = query(userRef, where("email", "==", user.email));
+
+// //   const querySnapshot = await getDocs(q);
+// //   let checkUser = [];
+// //   querySnapshot.forEach((doc) => {
+// //     checkUser.push({ id: doc.id, ...doc.data() });
+// //   });
+
+// //   if (checkUser.length > 0) return;
+
+// //   const userData = {
+// //     email: user.email,
+// //     tokens: 100,
+// //     model: "text-davinci-002",
+// //     isNewUser: true,
+// //     uid: user.uid,
+// //   };
+// //   await setDoc(doc(firestore, "users", user.uid), userData, { merge: true });
+// //   return user;
+// // };
+
+// // export const onUserSignedIn = async (user, router) => {
+// //   const userDoc = await doc(firestore, "users", user.uid).get();
+// //   const isNewUser = userDoc.data().isNewUser;
+// //   if (isNewUser) {
+// //     router.push("/");
+// //   } else {
+// //     router.push("/");
+// //   }
+// // };
--- components/LoginNavBar/index.tsx
+++ components/LoginNavBar/index.tsx
@@ -241,7 +241,7 @@ export default function LoginNavBar() {
             <p className="ms-1">Schedule</p>
           </Badge>
         </NavLink>
-        
+
         <Link
           href="/pricing"
           className={`bg-gradient-to-r  from-[#00C5D7] to-[#0077BE] rounded-md p-2 flex flex-row justify-between text-white`}
@@ -263,7 +263,7 @@ export default function LoginNavBar() {
               className={`${
                 theme === "light"
                   ? "flex flex-col absolute  p-2 mr-20 mt-44 w-32  md:h-38 h-50 bg-white z-10"
-                  : "flex flex-col absolute  p-2 mr-20 mt-44 w-32  md:h-38 h-50 bg-black z-10"
+                  : "flex flex-col absolute  p-2 mr-20 mt-44 w-32  md:h-38 h-50 bg-white z-10"
               }`}
             >
               <button
@@ -356,7 +356,9 @@ export default function LoginNavBar() {
                 <path d="M6.5 2a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1h-3zM11 8a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
                 <path d="M4.5 0A2.5 2.5 0 0 0 2 2.5V14a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2.5A2.5 2.5 0 0 0 11.5 0h-7zM3 2.5A1.5 1.5 0 0 1 4.5 1h7A1.5 1.5 0 0 1 13 2.5v10.795a4.2 4.2 0 0 0-.776-.492C11.392 12.387 10.063 12 8 12s-3.392.387-4.224.803a4.2 4.2 0 0 0-.776.492V2.5z" />
 
-                <span className="ms-2 text-left text-black">AI Creator Studio</span>
+                <span className="ms-2 text-left text-black">
+                  AI Creator Studio
+                </span>
               </button>
               <button
                 className="flex flex-row items-center mb-2"
