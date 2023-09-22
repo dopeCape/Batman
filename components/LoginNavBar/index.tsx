3fec5b718aaefb4c39891274f1cd0883e15c02a9
kanakdesai <kanakdesai1@gmail.com>
2023-09-22 17:13:23 +0530

    changes in dark and light mode

--- components/LoginNavBar/index.tsx
+++ components/LoginNavBar/index.tsx
@@ -282,7 +282,11 @@ export default function LoginNavBar() {
                   <path d="M6.5 2a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1h-3zM11 8a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
                   <path d="M4.5 0A2.5 2.5 0 0 0 2 2.5V14a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2.5A2.5 2.5 0 0 0 11.5 0h-7zM3 2.5A1.5 1.5 0 0 1 4.5 1h7A1.5 1.5 0 0 1 13 2.5v10.795a4.2 4.2 0 0 0-.776-.492C11.392 12.387 10.063 12 8 12s-3.392.387-4.224.803a4.2 4.2 0 0 0-.776.492V2.5z" />
                 </svg>
-                <span className="ms-2">Buy Tokens</span>
+                <span className={`ms-2 ${
+                theme === "light"
+                  ? "text-black"
+                  : "text-white"
+              } `}>Buy Tokens</span>
               </button>
               <button
                 className="flex flex-row items-center mb-2"
@@ -299,7 +303,11 @@ export default function LoginNavBar() {
                   <path d="M6.5 2a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1h-3zM11 8a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
                   <path d="M4.5 0A2.5 2.5 0 0 0 2 2.5V14a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2.5A2.5 2.5 0 0 0 11.5 0h-7zM3 2.5A1.5 1.5 0 0 1 4.5 1h7A1.5 1.5 0 0 1 13 2.5v10.795a4.2 4.2 0 0 0-.776-.492C11.392 12.387 10.063 12 8 12s-3.392.387-4.224.803a4.2 4.2 0 0 0-.776.492V2.5z" />
                 </svg>
-                <span className="ms-2">Contact us</span>
+                <span  className={`ms-2 ${
+                theme === "light"
+                  ? "text-black"
+                  : "text-white"
+              } `}>Contact us</span>
               </button>
               <div
                 className="flex flex-row border-t-2 items-center cursor-pointer w-full h-full"
@@ -324,7 +332,11 @@ export default function LoginNavBar() {
                     d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3z"
                   />
                 </svg>
-                <p className="ms-2">Log out</p>
+                <p  className={`ms-2 ${
+                theme === "light"
+                  ? "text-black"
+                  : "text-white"
+              } `}s>Log out</p>
               </div>
             </div>
           )}
